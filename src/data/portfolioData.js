b3fa657e5b317297919dfe1e7ebaf3be691b5ab9
PatrickDev2002 <patrick@fewlines.co>
2022-04-09 20:09:00 +0200

    Update portfolioData.js

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -24,6 +24,6 @@ export const portfolioData = [
     languagesIcons: ["fab fa-js", "fab fa-node", "fab fa-css3-alt"],
     source: "https://project-seven-chi.vercel.app/",
     info: "Premier projet en autonomie où je me suis amusé à mettre en oeuvre les compétences que j'ai pu acquérir durant les 2 premières semaines de stage, et ainsi tester de nouvelles choses pour moi à l'époque. Ce premier stage en entreprise m'a également permi d'aider la team 'front', en codant pour eux une page entière sur leur site web qui a été mergée il y a de ça peu de temps. Un aperçu est également disponible sur mon portfolio numérique.",
-    picture: "./media/piano.JPG", // monter une courte vidéo montrant chaque page du site, un défilement d'images serait approprié
+    picture: "./media/piano.jpg", // monter une courte vidéo montrant chaque page du site, un défilement d'images serait approprié
   },
 ];
